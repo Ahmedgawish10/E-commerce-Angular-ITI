7f3a5b065155fba482a49c383c3854aefe7628b6
ahmedgawish10 <ahmdgawish44@gmail.com>
2024-10-20 12:07:50 +0300

    fix confilict single route add to cart

--- src/app/private-card/private-card.component.ts
+++ src/app/private-card/private-card.component.ts
@@ -5,18 +5,23 @@ import { ApiProductsService } from '../services/api-products.service';
 import { NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CartService } from '../services/cart.service';
+import { AuthService } from '../services/auth.service';
+import { NgIf } from '@angular/common';
+
 import "./private-card.component.css"
 @Component({
   selector: 'app-private-card',
   standalone: true,
-  imports: [NgFor,FormsModule],
+  imports: [NgFor,FormsModule,NgIf],
   templateUrl: './private-card.component.html',
   styleUrl: './private-card.component.css'
 })
 export class PrivateCardComponent {
 // @Input() id ?: number;
-constructor(private activatedRoute : ActivatedRoute,private SelectesProductApi:ApiProductsService,private _CartService: CartService ){}
+constructor(private activatedRoute : ActivatedRoute,private SelectesProductApi:ApiProductsService,private _CartService: CartService,private _AuthService:AuthService ){}
 productDetails : any;
+popup: any;
+
 ngOnInit(){
   const selectedId= this.activatedRoute.snapshot.params['id']
   this.SelectesProductApi.getSelectedProduct(selectedId).subscribe((productSelected)=>this.productDetails=productSelected  )
@@ -26,9 +31,11 @@ ngOnInit(){
 arrProducts:any;
 qunt:any=0;
 addToCart(producti:any){
+  if(this._AuthService.isLoggedIn()){
+    
   console.log(this.productDetails.images[0]);
-  
   this._CartService.addToCart(producti)
+  }
 }
 increaseQuantity(productId: any): void {
   this._CartService.increaseQuantity(productId);
@@ -42,5 +49,27 @@ decreaseQuantity(productId: any): void {
   this._CartService.decreaseQuantity(productId);
   this.arrProducts = this._CartService.getItems();
 }
+
+
+isLoggedPopup(){
+  console.log(this._AuthService.isLoggedIn());
+  
+  if (this._AuthService.isLoggedIn()==false  ) {
+    if (this.popup==false) {
+      this.popup=!this.popup;
+    }else{
+      this.popup=!this.popup;
+    }
+  }else{
+    console.log("you logged already");
+    console.log(this._AuthService.isLoggedIn()); 
+  }
+ 
+  
+  
+}
+
+
+
 
 }
